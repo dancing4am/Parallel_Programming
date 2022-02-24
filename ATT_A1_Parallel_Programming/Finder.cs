3f86b32e7ad7cff753dc9419e96870d185b5c99e
Chowon Jung <56695129+dancing4am@users.noreply.github.com>
2022-02-23 19:05:58 -0500

    initial commit

---
+++ ATT_A1_Parallel_Programming/Finder.cs
@@ -0,0 +1,234 @@
+﻿/*
+ * FILE             :   Finder.cs
+ * PROJECT          :   ATT_A1_Parallel_Programming
+ * PROGRAMMER       :   Chowon Jung
+ * FIRST VERSION    :   2022-01-21
+ * DESCRIPTION      :   This file contains Finder class for password guessing process.
+ */
+
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace ATT_A1_Parallel_Programming
+{
+    /// <summary>
+    /// This class contains different methods for password guessing process,
+    /// including reporting the result.
+    /// </summary>
+    internal class Finder
+    {
+        /// <summary> minimum char property to guess as in ASCII </summary>
+        private int Min { get; }
+        /// <summary> maximum char property to guess as in ASCII </summary>
+        private int Max { get; }
+        /// <summary> counts time amount taken to guess the password </summary>
+        private Stopwatch stopwatch;
+        /// <summary> contains original password used to transport between methods </summary>
+        private char[] passwordChars = { };
+        /// <summary> contains found password used to transport between methods </summary>
+        private char[] foundChars = { };
+        /// <summary> number of tries to guess for current password </summary>
+        private int guessCount = 0;
+        /// <summary> indicates whether current guess exceeded the given time limit (<see cref="Validator.Limit"/>) </summary>
+        internal bool IsExceeded { get; set; } = false;
+
+        /// <summary>
+        /// Finder Constructor that allows setting up possible password ASCII code range
+        /// </summary>
+        /// <param name="min">minimum ASCII code possible for a password</param>
+        /// <param name="max">maximum ASCII code possible for a password</param>
+        internal Finder(int min, int max)
+        {
+            Min = min;
+            Max = max;
+            stopwatch = new Stopwatch();
+        }
+
+        /// <summary>
+        /// This method sets up affinity used for process and invokes a search method.
+        /// </summary>
+        /// <param name="password">user entered original password</param>
+        /// <param name="affinity">processor affinity</param>
+        /// <param name="limit">time limit for a whole guess in milliseconds given by user input</param>
+        internal void FindByAffinity(string password, int affinity, long limit)
+        {
+            // reset variables
+            guessCount = 0;
+            IsExceeded = false;
+            if (stopwatch == null)
+            {
+                stopwatch = new Stopwatch();
+            }
+            else
+            {
+                stopwatch.Reset();
+            }
+
+            // check for OS platform that allows processor affinity adjustment
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) || RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Process p = Process.GetCurrentProcess();
+                p.ProcessorAffinity = (IntPtr)affinity;
+                Console.WriteLine("using affinity " + p.ProcessorAffinity.ToString() + "...");
+            }
+
+            Console.WriteLine("Finding Password " + password);
+
+            // =========== these finding options are ordered by speed from largest to smallest.
+            // =========== you MUST COMMENT OUT other unused options when enabling one.
+
+            // Option #1 ******* Enabling this snippet will find the password
+            // sequentially from the largest digit in a single task.
+            passwordChars = password.ToCharArray();
+            foundChars = new char[passwordChars.Length];
+            Task task = Task.Run(() => FindOneByOneSequentially(password, limit));
+            task.Wait();
+
+            //// Option #2 ******* Enabling this snippet will find the password
+            //// with the same number of parallel tasks with the length of the password.
+            //passwordChars = password.ToCharArray();
+            //foundChars = new char[passwordChars.Length];
+            //Task[] taskList = new Task[passwordChars.Length];
+            //for (int i = 0; i < passwordChars.Length; i++)
+            //{
+            //    taskList[i] = new Task(() => FindIndividually(i, limit));
+            //    taskList[i].Start();
+            //    Console.WriteLine("Starting thread # " + i + " while password length is " + passwordChars.Length);
+            //}
+            //Task.WhenAll(taskList.ToArray());
+            //stopwatch.Stop();
+
+            //// Option #3 ******* Enabling this snippet will find the password
+            //// straight forward ascending in a single task counting from 0 to maximum number.
+            //Task task = Task.Run(() => FindAscending(password, limit));
+            //task.Wait();
+
+            // report user the result
+            if (IsExceeded)
+            {
+                Console.WriteLine("Time exceeded. Abort.");
+            }
+            else
+            {
+                Console.Write("Found Password is ");
+                Console.WriteLine(foundChars, 0, foundChars.Length);
+                Console.WriteLine("Solved in " + guessCount + " guesses, " + stopwatch.ElapsedTicks + " ticks, and "
+                    + TimeSpan.FromMilliseconds(stopwatch.ElapsedMilliseconds).TotalSeconds + " seconds.");
+            }
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// This method guesses the password from the largest digit to the smallest,
+        /// sequentially until the smallest digit is found.
+        /// </summary>
+        /// <param name="password">original full password to match</param>
+        /// <param name="limit">time limit for finding a whole password</param>
+        private void FindOneByOneSequentially(string password, long limit)
+        {
+            // set up and measure time amount taken
+            char[] passwordChar = password.ToCharArray();
+            if (!stopwatch.IsRunning)
+            {
+                stopwatch.Start();
+            }
+
+            // loop for each digit  
+            for (int i = 0; i < passwordChar.Length; i++)
+            {
+                // guess from minimum possible ASCII to maximum for each
+                for (int j = Min; j <= Max; j++)
+                {
+                    guessCount++;
+                    // if match found, record
+                    if (passwordChar[i] == j)
+                    {
+                        foundChars[i] = (char)j;
+                        break;
+                    }
+                    // if time limit is exceeded, abort
+                    else if (stopwatch.ElapsedMilliseconds >= limit)
+                    {
+                        IsExceeded = true;
+                        break;
+                    }
+                }
+                if (IsExceeded)
+                {
+                    break;
+                }
+            }
+            stopwatch.Stop();
+        }
+
+        /// <summary>
+        /// This method guesses a given single digit,
+        /// from the minimum possible ASCII to the maximum for each.
+        /// </summary>
+        /// <param name="index">an index of the password a task instance to guess</param>
+        /// <param name="limit">time limit for finding a whole password</param>
+        private void FindIndividually(int index, long limit)
+        {
+            // measure time amount taken
+            if (!stopwatch.IsRunning)
+            {
+                stopwatch.Start();
+            }
+
+            // guess from minimum possible ASCII to maximum for each
+            for (int i = Min; i <= Max; i++)
+            {
+                guessCount++;
+                // if match found, record
+                if ((passwordChars.Length > index) && (passwordChars[index] == i))
+                {
+                    foundChars[index] = (char)i;
+                    break;
+                }
+                // if time limit is exceeded, abort
+                else if (stopwatch.ElapsedMilliseconds >= limit)
+                {
+                    IsExceeded = true;
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// This method guesses password counting straight forward
+        /// from 0 to maximum possible long integer.
+        /// </summary>
+        /// <param name="password">original full password to match</param>
+        /// <param name="limit">time limit for finding a whole password</param>
+        private void FindAscending(string password, long limit)
+        {
+            // validate datatype of the password
+            if (long.TryParse(password, out long converted))
+            {
+                // measure time amount taken
+                if (!stopwatch.IsRunning)
+                {
+                    stopwatch.Start();
+                }
+                // count until max value
+                for (long i = 0; i < long.MaxValue; i++)
+                {
+                    guessCount++;
+                    // if match found, record
+                    if (converted == i)
+                    {
+                        stopwatch.Stop();
+                        foundChars = i.ToString().ToCharArray();
+                        break;
+                    }
+                    // if time limit is exceeded, abort
+                    else if (stopwatch.ElapsedMilliseconds >= limit)
+                    {
+                        IsExceeded = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
--- ATT_A1_Parallel_Programming/Program.cs
+++ ATT_A1_Parallel_Programming/Program.cs
@@ -1,108 +1,238 @@
-﻿
+﻿/*
+ * FILE             :   Program.cs
+ * PROJECT          :   ATT_A1_Parallel_Programming
+ * PROGRAMMER       :   Chowon Jung
+ * FIRST VERSION    :   2022-01-21
+ * DESCRIPTION      :   
+ */
 
-// See https://aka.ms/new-console-template for more information
 
+using ATT_A1_Parallel_Programming;
+
+/// <summary>
+/// This namespace includes classes and data used for parallel processing assignment.
+/// </summary>
 namespace ParallelProcessing
 {
-    internal enum InputCode
+    /// <summary>
+    /// Indicates current program status for flow decisions and report
+    /// </summary>
+    internal enum StatusCode
     {
-        Quit = 0,
-        Pass = 1,
-        NULLOrEmpty = 2,
-        Length = 3,
-        Format = 4
+        Quit = 0,           // to quit application
+        NULLOrEmpty = 1,    // input is null or empty
+        Length = 2,         // invalid password length
+        Format = 3,         // invalid password format
+        Time = 4,           // invalid time limit format
+        Next = 5,           // time limit is entered but no password yet
+        Pass = 6            // validation passed, move to guessing phase
     }
 
+    /// <summary>
+    /// This class contains a nested class and methods that invokes and validates user input.
+    /// </summary>
     public static class Program
     {
+        // password search area as ASCII for alphanumeric extensibility
+        /// <summary> minimum char property to guess as in ASCII </summary>
+        internal const int MIN = 48; // 48 as numeric 0
+        /// <summary> maximum char property to guess as in ASCII </summary>
+        internal const int MAX = 57; // 57 as numeric 9
+
+        /// <summary> minimum length for a password </summary>
+        const int MIN_LENGTH = 6;
+        /// <summary> maximum length for a password </summary>
+        const int MAX_LENGTH = 18;
+
+        /// <summary> key code for quit request </summary>
+        const string QUIT_CODE = "Q";
+        /// <summary> key code for re-entering time limit (<see cref="Validator.Limit"/>) request </summary>
+        const string TIME_CODE = "T";
+        /// <summary> affinity number for current process </summary>
+        const int AFF = 15;
+
         public static void Main(string[] args)
         {
+            // set up class instances
             Validator validator = new Validator();
-            if (validator.GetInput())
+            Finder finder = new Finder(MIN, MAX);
+
+            // repeat until quit request
+            while (validator.InvokeInput() > (int)StatusCode.Quit)
             {
-                // do guess
+                // do guess when validated only
+                if (validator.currentStatus == StatusCode.Pass)
+                {
+                    finder.FindByAffinity(validator.Password, AFF, validator.Limit);
+                    validator.currentStatus = StatusCode.Time;      // reset status for next guess
+                }
             }
-            // else quit
+            // else quit application
         }
-    }
 
-
-    internal class Validator
-    {
-        const int minLength = 6;
-        const int maxLength = 18;
-        const string quitCode = "Q";
-        readonly string[] announceMsgs = {
+        /// <summary>
+        /// This class is a nested class of <see cref="Program"/> that validates user input.
+        /// </summary>
+        private class Validator
+        {
+            /// <summary>messages displayed for user to inform current validation status</summary>
+            readonly string[] announceMsgs = {
             "Bye!",
-            "Processing . . .",
-            "Password cannot be empty.",
-            "Password digits cannot be less than 6 or larger than 18.",
-            "Passord should be made up of the digits 0 through 9 only."
-        };
+            "Your input cannot be empty.",
+            "Password digits cannot be less than " + MIN_LENGTH + " or larger than " + MAX_LENGTH + ".",
+            "Password must be made up of " + (char)MIN + " through " + (char)MAX + " only.",
+            "The time limit must be an integer larger than 0.",
+            "The search will not exceed the time limit you entered.",
+            ""
+            };
 
-        string currentMsg = "";
-
-        internal bool GetInput()
-        {
-            string? input = "";
-            int currentInputCode = (int)InputCode.Quit;
-
-            do
+            /// <summary>current message to be displayed</summary>
+            string currentMsg = String.Empty;
+            /// <summary>current application status</summary>
+            internal StatusCode currentStatus { get; set; } = StatusCode.Quit;
+            /// <summary>original user input password</summary>
+            internal string Password { get; set; } = null!;
+            /// <summary>time limit in milliseconds for a whole guesses for a password</summary>
+            internal long Limit { get; set; } = -1;
+            
+            /// <summary>
+            /// This method invokes user for time limit and password to guess.
+            /// </summary>
+            /// <returns>returns <see cref="currentStatus"/> as integer</returns>
+            internal int InvokeInput()
             {
-                Console.Clear();
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Please Enter a Password! (Press Q to Exit)");
-                Console.WriteLine("");
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.Write("The password");
-                Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.Write(" must ");
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.WriteLine("be:");
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine(" - made up of the digits 0 through 9 only");
-                Console.WriteLine(" - between 6 and 18 digits in length");
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("" + currentMsg);
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.Write("Your password is: ");
+                // reset password for each play
+                Password = string.Empty;
 
+                // if time limit has to be entered
+                if (Limit <= 0)
+                {
+                    // invoke for time limit input
+                    Console.Clear();
+                    Console.WriteLine("");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Please Enter a Time Limit in Milliseconds. (Enter " + QUIT_CODE + " to Exit)");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("" + currentMsg);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("The time limit is: ");
+
+                    // get and validate time limit input
+                    ValidateTimeLimit();
+                }
+                // if valid time limit is entered
+                else if (currentStatus <= StatusCode.Next)
+                {
+                    // invoke for password input
+                    Console.Clear();
+                    Console.WriteLine("");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Please Enter a Password. (Enter " + TIME_CODE + " to go back to Time Limit screen)");
+                    Console.WriteLine("");
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.Write("The password");
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.Write(" must ");
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("be:");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(" - made up of " + (char)MIN + " through " + (char)MAX + " only");
+                    Console.WriteLine(" - between " + MIN_LENGTH + " and " + MAX_LENGTH + " digits in length");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("" + currentMsg);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("Entered password is: ");
+
+                    // get and validate password
+                    if (ValidatePassword() <= (int)StatusCode.Quit)
+                    {
+                        // if quit requested, say bye
+                        Console.WriteLine(currentMsg);
+                    }
+                }
+
+                return (int)currentStatus;
+            }
+
+            /// <summary>
+            /// This method gets user input for time limit and validates it.
+            /// </summary>
+            private void ValidateTimeLimit()
+            {
+                // get user input
+                string? input = string.Empty;
                 input = Console.ReadLine();
-            } while (ValidateInput(ref input, currentInputCode) > (int)InputCode.Pass);
 
-            if (currentInputCode <= (int)InputCode.Quit)
-            {
-                Console.WriteLine(currentMsg);
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-        }
+                // validate user input
+                if (string.IsNullOrEmpty(input))
+                {
+                    currentStatus = StatusCode.NULLOrEmpty;
+                }
+                else if (input.Equals(QUIT_CODE, StringComparison.OrdinalIgnoreCase))
+                {
+                    // if requested to quit, set flag
+                    currentStatus = StatusCode.Quit;
+                }
+                else if ((!long.TryParse(input, out long o)) || (o <= 0))
+                {
+                    currentStatus = StatusCode.Time;
+                }
+                else
+                {
+                    // if valid, record and set progress flag
+                    Limit = long.Parse(input);
+                    currentMsg = string.Empty;
+                    currentStatus = StatusCode.Next;
+                }
 
-        private int ValidateInput(ref string? input, int currentInputCode)
-        {
-            if (string.IsNullOrEmpty(input))
-            {
-                currentInputCode = (int)InputCode.NULLOrEmpty;
-
-            }
-            else if (input.Equals(quitCode, StringComparison.OrdinalIgnoreCase))
-            {
-                currentInputCode = (int)InputCode.Quit;
-            }
-            else if ((input.Length < minLength) || (input.Length > maxLength))
-            {
-                currentInputCode = (int)InputCode.Length;
-            }
-            else if (!Int32.TryParse(input, out _))
-            {
-                currentInputCode = (int)InputCode.Format;
+                // set announce message for report
+                currentMsg = announceMsgs[(int)currentStatus];
             }
 
-            currentMsg = announceMsgs[currentInputCode];
-            return currentInputCode;
+            /// <summary>
+            /// This method gets and validates user input password.
+            /// </summary>
+            /// <returns>returns <see cref="currentStatus"/> as integer.</returns>
+            private int ValidatePassword()
+            {
+                // get user input
+                string? input = string.Empty;
+                input = Console.ReadLine();
+
+                // validate user input
+                if (string.IsNullOrEmpty(input))
+                {
+                    currentStatus = StatusCode.NULLOrEmpty;
+
+                }
+                else if (input.Equals(TIME_CODE, StringComparison.OrdinalIgnoreCase))
+                {
+                    // if requested for time limit input, set flags
+                    Limit = -1;
+                    currentStatus = StatusCode.Time;
+                }
+                else if ((input.Length < MIN_LENGTH) || (input.Length > MAX_LENGTH))
+                {
+                    currentStatus = StatusCode.Length;
+                }
+                else
+                {
+                    Password = input;
+                    currentStatus = StatusCode.Pass;
+
+                    for (int i = 0; i < input.Length; i++)
+                    {
+                        if ((input[i] < MIN) || (input[i] > MAX))
+                        {
+                            currentStatus = StatusCode.Format;
+                        }
+                    }
+                }
+
+                // set announce message for report
+                currentMsg = announceMsgs[(int)currentStatus];
+                return (int)currentStatus;
+            }
         }
     }
 }
